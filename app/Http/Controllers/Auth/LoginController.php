6a158c4dfc79981d145b438335f52d08bb95c463
TimothyMee <timothy33.tf@gmail.com>
2018-01-13 22:13:58 +0100

    second commit

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,11 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Cache;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Contracts\Auth\Authenticatable;
-use App\Http\Requests\ValidateSecretRequest;
+
 class LoginController extends Controller
 {
     /*
@@ -39,41 +35,5 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
-    }
-
-    private function authenticated(Request $request, Authenticatable $user)
-    {
-        if ($user->google2fa_secret) {
-            Auth::logout();
-
-            $request->session()->put('2fa:user:id', $user->id);
-
-            return redirect('two-factor/validate');
-        }
-
-        return redirect()->intended($this->redirectTo);
-    }
-
-    public function getValidateToken()
-    {
-        if (session('2fa:user:id')) {
-            return view('two-factor/validate');
-        }
-
-        return redirect('login');
-    }
-
-    public function postValidateToken(ValidateSecretRequest $request)
-    {
-        /*get user id*/
-        $userId = $request->session()->pull('2fa:user:id');
-        $key    = $userId . ':' . $request->totp;
-
-        Cache::add($key, true, 4);
-
-        /*login and redirect user*/
-        Auth::loginUsingId($userId);
-
-        return redirect()->intended($this->redirectTo);
     }
 }
